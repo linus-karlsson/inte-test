320309e7fd6ce1adcc648bfecddd59e08703cfe9
Patrick Westberg <westbergpatrick@gmail.com>
2023-09-08 10:45:18 +0200

    Patrick Commit

---
+++ src/main/java/com/inte/app/Counter.java
@@ -0,0 +1,10 @@
+package com.inte.app;
+
+public class Counter {
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println(i);
+        i++;
+        System.out.println(i);
+    }
+}
